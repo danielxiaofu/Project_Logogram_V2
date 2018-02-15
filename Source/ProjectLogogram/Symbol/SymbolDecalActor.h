bea44715984c1102e486a5ed8523ada76001a5c7
Yixiao Yang <yangyixiao93@gmail.com>
2018-02-14 23:51:41 -0500

    Make stone plate light shaft

---
+++ Source/ProjectLogogram/Symbol/SymbolDecalActor.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DecalActor.h"
+#include "SymbolDecalActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLOGOGRAM_API ASymbolDecalActor : public ADecalActor
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item Info")
+	int32 ID;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item Info")
+	int32 Type;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item Info")
+	FName Name;
+
+};
