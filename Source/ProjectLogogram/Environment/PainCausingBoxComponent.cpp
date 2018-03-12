e4935f60e25b000a86ef60dbeca37f7b780ec18f
Yixiao Yang <yangyixiao93@gmail.com>
2018-03-11 21:42:26 -0400

    Add trap to maze

---
+++ Source/ProjectLogogram/Environment/PainCausingBoxComponent.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PainCausingBoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "TimerManager.h"
+#include "GameFramework/DamageType.h"
+#include "GameFramework/Pawn.h"
+
+void UPainCausingBoxComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	GetOwner()->OnEndPlay.AddDynamic(this, &UPainCausingBoxComponent::OwnerEndPlay);
+	OnComponentBeginOverlap.AddDynamic(this, &UPainCausingBoxComponent::ActorEnter);
+}
+
+void UPainCausingBoxComponent::OwnerEndPlay(AActor* Actor, EEndPlayReason::Type EndPlayReason)
+{
+	GetOwner()->GetWorldTimerManager().ClearTimer(TimerHandle_PainTimer);
+}
+
+void UPainCausingBoxComponent::ActorEnter(UPrimitiveComponent* OtherComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult &SweepResult)
+{
+	if (bPainCausing && bEntryPain && OtherActor->bCanBeDamaged)
+	{
+		CausePainTo(OtherActor);
+	}
+	// Start timer if none is active
+	if (!GetOwner()->GetWorldTimerManager().IsTimerActive(TimerHandle_PainTimer))
+	{
+		GetOwner()->GetWorldTimerManager().SetTimer(TimerHandle_PainTimer, this, &UPainCausingBoxComponent::PainTimer, PainInterval, true);
+	}
+}
+
+void UPainCausingBoxComponent::CausePainTo(AActor * Other)
+{
+	if (DamagePerSec > 0.f)
+	{
+		for (TSubclassOf<UDamageType> DamageClass : DamageTypes)
+		{
+			Other->TakeDamage(DamagePerSec * PainInterval, FDamageEvent(DamageClass), nullptr, GetOwner());
+		}
+	}
+}
+
+void UPainCausingBoxComponent::PainTimer()
+{
+	if (bPainCausing)
+	{
+		TArray<AActor*> OverlappingActors;
+		GetOverlappingActors(OverlappingActors, APawn::StaticClass());
+
+		for (AActor * const A : OverlappingActors)
+		{
+			if (A && A->bCanBeDamaged && !A->IsPendingKill())
+			{
+				CausePainTo(A);
+			}
+		}
+
+		if (OverlappingActors.Num() == 0)
+		{
+			GetOwner()->GetWorldTimerManager().ClearTimer(TimerHandle_PainTimer);
+		}
+	}
+}
+
+
--- Source/ProjectLogogram/Environment/PainCausingBoxComponent.h
+++ Source/ProjectLogogram/Environment/PainCausingBoxComponent.h
@@ -3,25 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/PrimitiveComponent.h"
-#include "PainCausingVolumeComponent.generated.h"
+#include "Components/BoxComponent.h"
+#include "PainCausingBoxComponent.generated.h"
 
+class AActor;
 class UDamageType;
 
 /**
  * 
  */
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
-class PROJECTLOGOGRAM_API UPainCausingVolumeComponent : public UPrimitiveComponent
+class PROJECTLOGOGRAM_API UPainCausingBoxComponent : public UBoxComponent
 {
 	GENERATED_BODY()
-
+	
 protected:
 
 	/** Whether volume currently causes damage. */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PainCausingVolume)
 	uint32 bPainCausing : 1;
-	
+
 	/** Damage done per second to actors in this volume when bPainCausing=true */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PainCausingVolume)
 	float DamagePerSec;
@@ -38,11 +39,28 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PainCausingVolume)
 	uint32 bEntryPain : 1;
 
+	/** Handle for efficient management of OnTimerTick timer */
+	FTimerHandle TimerHandle_PainTimer;
+
+	UFUNCTION()
+	void OwnerEndPlay(AActor* Actor, EEndPlayReason::Type EndPlayReason);
+
+	UFUNCTION()
+	void ActorEnter(UPrimitiveComponent* OtherComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep,
+		const FHitResult &SweepResult);
+
+	/** damage overlapping actors if pain causing. */
+	virtual void CausePainTo(AActor* Other);
+
 	/** Damage overlapping actors if pain-causing. */
 	void PainTimer();
 
 public:
 
 	void BeginPlay() override;
-
+	
 };
